97e81c89be6107e093342037364c0adc5c7a425b
Новицкий Вячеслав <Neffes1987@gmail.com>
2020-03-15 21:45:51 +0300

    добавил экран сцены

---
+++ app/src/main/java/com/example/masterhelper/expand_text_description.java
@@ -0,0 +1,29 @@
+package com.example.masterhelper;
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class expand_text_description extends Fragment {
+
+
+  public expand_text_description() {
+    // Required empty public constructor
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_expand_text_description, container, false);
+  }
+
+}
