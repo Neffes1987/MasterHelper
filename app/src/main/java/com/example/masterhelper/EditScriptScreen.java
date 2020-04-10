2e8dbb3bfea2042047761ba61e91a8e379f18b19
Новицкий Вячеслав <Neffes1987@gmail.com>
2020-04-10 23:49:05 +0300

    привел код в порядок

---
+++ app/src/main/java/com/example/masterhelper/EditScriptScreen.java
@@ -0,0 +1,13 @@
+package com.example.masterhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditScriptScreen extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_edit_script_screen);
+  }
+}
---
+++ app/src/main/java/com/example/masterhelper/MusicSettingsScreen.java
@@ -0,0 +1,13 @@
+package com.example.masterhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicSettingsScreen extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_music_settings_screen);
+  }
+}
